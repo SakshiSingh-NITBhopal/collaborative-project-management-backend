6b76e08a123f9cb0f59c740b60a2089dadebb7ef
SakshiSingh-NITBhopal <sakshisinghnitbhopal@gmail.com>
2025-10-22 11:02:46 +0530

    add user schema in mongoose

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,11 +1,11 @@
 import {ApiResponse} from "../utils/api-response.js"
-import { asyncHandler } from "../utils/ayncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 //this fmethod will send response when the health check api endpoint is hitted
 const healthCheck = asyncHandler((req, res) => {
     res
         .status(200)
-        .json(new ApiResponse(200, {message : "server is running"}));
+        .json(new ApiResponse(200, {message : "server is still running"}));
 })
 
 export {healthCheck};
